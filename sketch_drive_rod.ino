276b78a607829c0a6a7dc3022fb5d489d05cd9cc
George Barta <george.barta@gmail.com>
2016-06-20 00:13:50 +0000

    Use 2-9 to set the speed for one full rotation

--- sketch_drive_rod.ino
+++ sketch_drive_rod.ino
@@ -1,27 +1,64 @@
 #include <SoftwareSerial.h>
-#include <comm.h>
 #include <Arduino.h>
 
-const int x_step = 2;
-const int x_dir = 5;
-const int disable = 8;
+const int X_STEP_PIN = 2;
+const int X_DIR_PIN = 5;
+const int DISABLE_PIN = 8;
 
-//const int steps_per_rotation = 200 * 4;
-const int steps_per_rotation = 40;
+/*
+  With all 3 jumpers in the driver is configured to run at 1/16 microstepping
+*/
+const int STEPS_PER_ROTATION = 200 * 16;
 
-int hold_high = 300;
-int hold_low = 700;
-int t;
+int step_time = 500;
+int is_active;
 
 void setup() {
-  pinMode(x_step, OUTPUT);
-  pinMode(x_dir, OUTPUT);
-  pinMode(disable, OUTPUT);
+  pinMode(X_STEP_PIN, OUTPUT);
+  pinMode(X_DIR_PIN, OUTPUT);
+  pinMode(DISABLE_PIN, OUTPUT);
   Serial.begin(57600);
   Serial.println("RoboFoos Ready!");
-  //send: 07766544444517654444460776654444451765444444670776654444451765444446077665444445176544444467
-  //digitalWrite(x_dir, HIGH);
-  t = 0;
+  digitalWrite(X_DIR_PIN, HIGH);
+  is_active = 0;
+}
+
+void go(int steps, int dir) {
+  if(dir <= 0) {
+    digitalWrite(X_DIR_PIN, LOW);
+  } else {
+    digitalWrite(X_DIR_PIN, HIGH);
+  }
+
+  for (int i = 0; i < steps; i++){    
+    int hold_time = step_time;
+    if(i==0 || i == steps-1){
+      hold_time = 800;
+    } else if (i == 1 || i == steps-2) {
+      hold_time = 750;
+    } else if (i == 2 || i == steps-3) {
+      hold_time = 600;
+    } else if (i == 3 || i == steps-4) {
+      hold_time = 500;
+    } else if (i == 4 || i == steps-5) {
+      hold_time = 400;
+    } else if (i == 5 || i == steps-6) {
+      hold_time = 300;
+    } else if (i == 6 || i == steps-7) {
+      hold_time = 200;
+    } else if (i == 7 || i == steps-8) {
+      hold_time = 100;
+    } else if (i == 8 || i == steps-9) {
+      hold_time = 70;
+    } else if (i == 9 || i == steps-10) {
+      hold_time = 50;
+    }
+    digitalWrite(X_STEP_PIN, HIGH);
+    delayMicroseconds(20);
+    digitalWrite(X_STEP_PIN, LOW);
+    delayMicroseconds((hold_time)-20);      
+  }
+  //digitalWrite(DISABLE_PIN, HIGH);  
 }
 
 void loop() {
@@ -31,34 +68,22 @@ void loop() {
     Serial.print("N is ");
     Serial.println(n);
     if(n == 0) {
-      digitalWrite(x_dir, HIGH);
-    } else if(n==1){
-      digitalWrite(x_dir, LOW);
-    } else if(n==2){
-      delay(1000);
-    } else if(3 < n && n <= 9) {
-      //hold_high = n * 100;
-      hold_low = n * 100;
-      t+=1;
+      is_active = 0;
+    } 
+    else if(1 < n && n <= 9) {
+      step_time = n * 30;
+      is_active = 1;
     }
     Serial.print("Running with ");
-    Serial.println(hold_low);
+    Serial.println(step_time);
   }
-  if(t > 0) {
-
-    digitalWrite(disable, LOW);
-    //digitalWrite(x_dir, HIGH);
-
-    for (int i = 0; i < steps_per_rotation; i++){    
-      digitalWrite(x_step, HIGH);
-      delayMicroseconds(hold_high);
-      digitalWrite(x_step, LOW);
-      delayMicroseconds(hold_low);      
-    }
-    //digitalWrite(disable, HIGH);  
-  
-    t -= 1;
+  if(is_active > 0) {
+    digitalWrite(DISABLE_PIN, LOW);
+      go(STEPS_PER_ROTATION, 0);
+      go(STEPS_PER_ROTATION, 1);
+    digitalWrite(DISABLE_PIN, HIGH);
   } else {
-    digitalWrite(disable, HIGH); 
+    digitalWrite(DISABLE_PIN, HIGH); 
   }
 }
+
